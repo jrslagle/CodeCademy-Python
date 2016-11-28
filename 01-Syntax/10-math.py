e97585b86e11706ce5e0156ea50fee46d4845318
James Slagle <jrslagle@gmail.com>
2016-11-27 21:10:22 -0700

    lesson 1 complete

---
+++ 01-Syntax/10-math.py
@@ -0,0 +1,5 @@
+# Set count_to equal to the sum of two big numbers
+
+count_to = 1253 +   2335234
+
+print(count_to)
---
+++ 01-Syntax/13-comprehensive.py
@@ -0,0 +1,9 @@
+# my first free form program
+
+monty = True
+
+python = 1.234
+
+monty_python = python ** 2
+
+print(monty_python)
